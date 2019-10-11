71f787fd818e30b3ed52f0bed8892503f2383e86
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-10-11 16:05:28 +0800

    add CORS

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,7 +1,10 @@
 # coding:utf-8
 from flask import Flask,request,render_template
+from flask_cors import CORS
 from app.controllers.swap import swap
 app=Flask(__name__)
+CORS(app)
+
 app.register_blueprint(swap, url_prefix='/swap')
 
 
