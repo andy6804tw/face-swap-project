7d057960c9227a4bd6ddbdf96329408eb32b0123
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-10-11 10:13:10 +0800

    add post image api

---
+++ API/index.py
@@ -0,0 +1,27 @@
+
+from flask import Flask,jsonify,request
+import model
+
+app = Flask(__name__)
+stores = [{
+    'name': 'Elton',
+    'items': [{'name':'my item 1', 'price': 30 }],
+    },
+    {
+    'name': 'Elton',
+    'items': [{'name':'my item 2', 'price': 15 }],
+    },
+]
+#get /store
+@app.route('/store')
+def get_stores():
+    return jsonify(stores)
+
+#post /store/<name> data: {name :}
+@app.route('/store' , methods=['POST'])
+def create_item_in_store():
+    request_data = request.get_json()
+    return jsonify({'result':model.imagePost(request_data)})
+
+
+app.run(debug=True,port=5000)
---
+++ API/model.py
@@ -0,0 +1,6 @@
+from flask import Flask,jsonify,request
+
+def imagePost(res):
+  image1=res['image1']
+  image2=res['image2']
+  return (image1*image2)
