38ee4562f4b25dcb7a432a747308c450be380f6d
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-10-12 22:26:00 +0800

    design demo layout

---
+++ demo/js/index.js
@@ -0,0 +1,66 @@
+const fileUploader = document.querySelector('#file-uploader');
+const dst_img = document.getElementById("dst_img");
+
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+});
+fileUploader.addEventListener('change', async (e) => {
+
+  // console.log(e.target.files[0]); // get file object
+});
+
+
+
+const postImage = async () => {
+  const file1 = await toBase64(fileUploader.files[0]);
+  const image1 = file1.split(",")[1];
+  const file2 = await toBase64(dst_img.files[0]);
+  const image2 = file2.split(",")[1];
+  // console.log(image1);
+  axios.post(`http://localhost:5000/swap`, {
+    image1,
+    image2
+  })
+    .then((response) => {
+      var dataObject = response.data;
+      // POST success
+      const responseImg = dataObject.result.split("'")[1];
+      document.getElementById('image').src = `data:image/jpeg;base64,${responseImg}`;
+      window.location.href = '#anchor2';
+    },
+      (error) => {
+        var message = error.response.data.message;
+      }
+    );
+}
+
+/** Comparison of image */
+slider = new juxtapose.JXSlider('.juxtapose',
+  [
+    {
+      src: './../FaceSwap-model/imgs/test11.jpg',
+      label: 'Before',
+      // credit: 'Image Credit'
+    },
+    {
+      src: './../API/FaceSwap/results/myOut.jpg',
+      label: 'After',
+      // credit: "Image Credit"
+    }
+  ],
+  {
+    animate: true,
+    showLabels: true,
+    showCredits: true,
+    startingPosition: "50%",
+    makeResponsive: true
+  });
+
+
+setTimeout(function () {
+  document.getElementsByClassName("jx-knightlab")[0].remove();
+}, 500)
+
