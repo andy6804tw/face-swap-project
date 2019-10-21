5fd09a882b397e1dae4882943cb81e0103ce5052
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-10-22 00:27:33 +0800

    add upload video and img

--- API/app/controllers/upload.py
+++ API/app/controllers/upload.py
@@ -8,8 +8,14 @@ upFile = UploadSet('upFile',extensions=AllExcept(()))
 @upload.route('', methods=['GET','POST'])
 def uploadFile():
   if request.method == 'POST' and 'file'  in request.files:
-        filename = upFile.save(request.files['file'])
-        file_url = upFile.url(filename)
-        return 'don'
+    request.files['file'].filename='srcVideo.'+request.files['file'].filename.split('.')[1]
+    filename = upFile.save(request.files['file'])
+    # file_url = upFile.url(filename)
+    return 'don'
+  elif request.method == 'POST' and 'fileImage'  in request.files:
+    request.files['fileImage'].filename='srcImage.'+request.files['fileImage'].filename.split('.')[1]
+    filename = upFile.save(request.files['fileImage'])
+    # file_url = upFile.url(filename)
+    return 'don'
   else:
     return 'hi'
